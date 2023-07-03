78728c401039ea1d95d4e757445a124e13c24488
Bharath Reddy <62090066+nameminister@users.noreply.github.com>
2023-07-03 23:24:46 +0530

    Plugin Stater File

---
+++ wp-user-flow.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP User Flow
+ * Description: Extend your WordPress website to include responsive and advanced user dashboard.
+ * Version: 1.0.0
+ * Author: Bharath Kumar Reddy
+ */
+if (!function_exists('dev_onl_wp_user_flow')) {
+	# code...
+	function dev_onl_wp_user_flow(){
+		# code...
+	}
+}
